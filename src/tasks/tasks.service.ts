6d6c5041252e4700523af362ecb6e0b27ef31fb1
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-13 15:40:11 +0300

    tasks: dto`s

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuidv1 }  from 'uuid';  
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Task[] = [];
 
-    getAllTasks(){
+    getAllTasks(): Task[] {
         return this.tasks;
+    }
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+        const { title, description } = createTaskDto;
+
+        const task: Task = {
+            id: uuidv1(),
+            title, 
+            description,
+            status: TaskStatus.OPEN,
+        };
+        
+        this.tasks.push(task);
+        return task;
     }
 }
