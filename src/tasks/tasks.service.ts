9b3e9d3924fb30deb8e7b1b3c9f709f6b8aa8fbc
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-14 12:14:31 +0300

    tasks: tasks service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 }  from 'uuid';  
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,7 +12,11 @@ export class TasksService {
     }
 
     getTaskById(id: string): Task {
-        return this.tasks.find(task => task.id === id);
+        const task = this.tasks.find(task => task.id === id);
+        if (!task){
+            throw new NotFoundException(`Task with id ${id} was not found`);
+        }
+        return task;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task {
@@ -30,7 +34,8 @@ export class TasksService {
     }
 
     deleteTask(id: string): void {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const found = this.getTaskById(id);
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task {
