bc0954a82013362d806f3dcad7a00dfb8feff797
Anton Kinalevskiy <kinalevskiy@gmail.com>
2020-04-23 16:57:41 +0300

    refactor: configuration files

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'taskmanager',
+    type: dbConfig.type,
+    host: process.env.HOSTNAME || dbConfig.host,
+    port: process.env.PORT || dbConfig.port,
+    username: process.env.DB_USERNAME || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{ts,js}'],
-    synchronize: true
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
-  const port = 3000;
+  const serverConfig = config.get('server');
+  const port = process.env.PORT || serverConfig.port || 3000;
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('bootstrap');
   await app.listen(port).then( () => { logger.log(`Application started on port ${port}`)});
